912db643bf18cedbabf4cb844d810e57d7792b44
Kyohei Nakafuji <kyohei.nakafuji@ssie.jp>
2016-12-20 14:52:04 +0900

    return json sample

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
-import os
-from bottle import route, run
+import os,json
+from bottle import route, run, request, response
 
 @route("/")
-def hello_world():
-        return "Hello World update"
+def hello():
+	return "hello"
 
-@route("/hello")
-def hello_world():
-        return "hello"
+@route('/search',method='GET')
+def search():
+#    userId = request.params('userId')
+    body = json.dumps({'message': 'helloAjax'})
+    response.status = 200
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps(body)
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#run(host="localhost", port=int(os.environ.get("PORT", 5000)))
