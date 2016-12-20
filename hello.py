3d0dcc373f0077ea014f2bdf7bcb14aecc250d8e
Kyohei Nakafuji <kyohei.nakafuji@ssie.jp>
2016-12-20 16:20:40 +0900

    return json sample2

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os,json
-from bottle import route, run, request, response
+from bottle import route, run, request, response, HTTPResponse
 
 @route("/")
 def hello():
@@ -8,10 +8,21 @@ def hello():
 @route('/search',method='GET')
 def search():
 #    userId = request.params('userId')
-    body = json.dumps({'message': 'helloAjax'})
+    body = json.dumps([{'message':'helloAjax'}])
     response.status = 200
-    response.headers['Content-Type'] = 'application/json'
-    return json.dumps(body)
+    response.content_type = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+#    response.access_control_allow_origin = '*'
+    
+#    response.set_header('Access-Control-Allow-Origin','*')
+#    response.add_header('Content-Type','application/json')
+#    response.headers['Access-Control-Allow-Origin'] = '*'
+#    response.headers['Content-Type'] = 'application/json'
+#    r = HTTPResponse(status=200, body=body)
+#    r.set_header("Content-Type", "application/json")
+#    r.add_header("Access-Control-Allow-Origin","*")
+#    return r
+    return body
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-#run(host="localhost", port=int(os.environ.get("PORT", 5000)))
+#run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+run(host="localhost", port=int(os.environ.get("PORT", 5000)))
